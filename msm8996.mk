578e48f6f43edb29ae09cc677541e400241699b6
CrisBal <crisbaladim@yahoo.gr>
2021-01-31 12:41:30 +0200

    msm8996-common: Do more legion-ify

--- msm8996.mk
+++ msm8996.mk
@@ -32,7 +32,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-legion
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
