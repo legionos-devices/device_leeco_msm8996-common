LineageOsMaxPlus/android_device_leeco_msm8996-common

msm8996-common: add max_plus
* max_plus will have unified builds with x2

LeEco Le Max Pro
ro.product.model=Letv X910
ro.product.brand=LeEco
ro.product.name=LeMaxPro_CN
ro.product.device=max_plus

Change-Id: I222c86f07082c7c539fe8995586dd8799e9edc2d

init/init_leeco_msm8996.cpp
@@ -149,6 +149,18 @@ void init_target_properties()
             property_set("ro.telephony.default_network", "10,10");
             unknownDevice = 0;
         }
+        else if (!strncmp(device.c_str(), "max_plus", 8)) {
+            // This is LEX910
+            property_override_dual("ro.product.device", "ro.product.vendor.device", "max_plus");
+            property_override_dual("ro.product.model", "ro.product.vendor.model", "LEX910");
+            property_override_dual("ro.product.name", "ro.product.vendor.name", "LeMaxPro_CN");
+            // Dual SIM
+            property_set("persist.radio.multisim.config", "dsds");
+            property_set("ro.telephony.default_network", "10,10");
+            // NFC
+            property_set("persist.nfc.smartcard.config", "SIM1,SIM2,eSE1");
+            unknownDevice = 0;
+        }
     }
     else {
         LOG(ERROR) << "Unable to read DEVINFO from " << DEVINFO_FILE;

legionos-devices/device_leeco_msm8996-common

init/init_leeco_msm8996.cpp
@@ -178,6 +178,18 @@ void vendor_load_properties()
         property_override("ro.telephony.default_network", "10,10");
         unknownDevice = false;
     }
+    else if (device == "max_plus") {
+        // This is LEX910
+        property_overrride_triple("ro.product.device", "ro.product.system.device", "ro.product.vendor.device", "max_plus");
+        property_overrride_triple("ro.product.model", "ro.product.system.model", "ro.product.vendor.model", "LEX910");
+        property_overrride_triple("ro.product.name", "ro.product.system.name", "ro.product.vendor.name", "LeMaxPro_CN");
+        // Dual SIM
+        property_override("persist.radio.multisim.config", "dsds");
+        property_override("ro.telephony.default_network", "10,10");
+        // NFC
+        property_override("persist.nfc.smartcard.config", "SIM1,SIM2,eSE1");
+        unknownDevice = 0;
+    }
     else {
         LOG(ERROR) << "Unable to set DEVINFO from ro.leeco.devinfo prop";
     }